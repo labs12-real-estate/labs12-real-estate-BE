f3c424f13c6037953fb3c85890a299b9c72ab683
Anthony Venturini <anthonydventurini@gmail.com>
2019-06-14 18:45:23 -0700

    rewind

--- houses/housesRouter.js
+++ houses/housesRouter.js
@@ -81,21 +81,21 @@ router.post('/getvalue', (req, res) => {
       complete_address = complete_address.slice(0, -5);
       console.log(complete_address) // remove 5 characters ", USA" at the end
       // complete_address = complete_address.slice(0, -6) + ',' + complete_address.slice(-6, complete_address.length); // insert comma between state and zipcode
-      // axios
-      //   .post('JustReturnParcel-env.kws5pjgawj.us-east-2.elasticbeanstalk.com', { address: complete_address })
-      //   .then(data => {
-      //     data.data.address = complete_address;
-      //     res.status(200).json(data.data);
-      //   })
-      //   .catch(err => {
-      //     return res.status(500).json({ err });
-      //   });
-      zillow.get('GetDeepSearchResults', {address: complete_address, citystatezip: complete_address.slice(-2)}).then(data => {console.log(data);res.status(200).json(data.addressWithUpdates)
+      axios
+        .post('JustReturnParcel-env.kws5pjgawj.us-east-2.elasticbeanstalk.com', { address: complete_address })
+        .then(data => {
+          data.data.address = complete_address;
+          res.status(200).json(data.data);
         })
-    })
-    .catch(err => {
-      return res.status(500).json({ err });
-    });
+        .catch(err => {
+          return res.status(500).json({ err });
+        });
+    //   zillow.get('GetDeepSearchResults', {address: complete_address, citystatezip: complete_address.slice(-2)}).then(data => {console.log(data);res.status(200).json(data.addressWithUpdates)
+    //     })
+    // })
+    // .catch(err => {
+    //   return res.status(500).json({ err });
+    // });
 });
 
 router.post('/getprecisevalue', (req, res) => {
@@ -109,4 +109,4 @@ router.post('/getprecisevalue', (req, res) => {
     });
 });
 
-// module.exports = router;
+module.exports = router;
